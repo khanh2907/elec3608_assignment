856053b90f848261776ab824a76d21dba7efe16c
Khanh Nguyen <khanh@intersect.org.au>
2013-11-11 20:44:43 +1100

    Add VHDL files and Report

---
+++ PS_SPIM/ps_control.vhd
@@ -0,0 +1,50 @@
+--
+-- control unit. simply implements the truth table for a small set of
+-- instructions 
+--
+--
+
+Library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity control is
+port(opcode: in std_logic_vector(5 downto 0);
+     RegDst, MemRead, MemToReg, MemWrite :out  std_logic;
+     ALUSrc, RegWrite, Branch: out std_logic;
+     ALUOp: out std_logic_vector(1 downto 0));
+end control;
+
+architecture behavioral of control is
+
+signal rformat, lw, sw, beq  :std_logic; -- define local signals
+				    -- corresponding to instruction
+				    -- type 
+ begin 
+--
+-- recognize opcode for each instruction type
+-- these variable should be inferred as wires	 
+
+	rformat 	<=  '1'  WHEN  Opcode = "000000"  ELSE '0';
+	Lw          <=  '1'  WHEN  Opcode = "100011"  ELSE '0';
+ 	Sw          <=  '1'  WHEN  Opcode = "101011"  ELSE '0';
+   	Beq         <=  '1'  WHEN  Opcode = "000100"  ELSE '0';
+
+--
+-- implement each output signal as the column of the truth
+-- table  which defines the control
+--
+
+RegDst <= rformat;
+ALUSrc <= (lw or sw) ;
+
+MemToReg <= lw ;
+RegWrite <= (rformat or lw);
+MemRead <= lw ;
+MemWrite <= sw;	   
+Branch <= beq;
+
+
+ALUop(1 downto 0) <= "01" WHEN Beq = '1' ELSE rformat & '0';
+
+
+end behavioral;
